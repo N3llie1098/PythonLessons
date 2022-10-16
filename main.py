e8aa06f86496492c96075e543e5347ce9fcdfdd7
N3llie1098 <115923566+N3llie1098@users.noreply.github.com>
2022-10-16 10:48:14 +0000

    Added class "Human"

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-print ('Hello world')
-for i in range(5):
-  print ('E')
+class Human:
+  def __init__(self):
+    self.name = 'None'
+    self.age = 0
+    self.gender = 'None'
+  def introduce(self):
+    print('Hello, my name is ', self.name)
+  def add_info(self):
+    self.name = input('Name: ')
+    self.age = int(input('Age: '))
+    self.gender = input('Gender: ')
+
+obj = Human()
+obj.introduce()
+obj.add_info()
+obj.introduce()
